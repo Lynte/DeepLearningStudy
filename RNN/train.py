37743e8a93437d4d7a24acf7cbdbd8008e0b2a5c
Lynte <lillynte@gmail.com>
2016-10-12 11:33:47 +0900

    add ptb

---
+++ RNN/train.py
@@ -0,0 +1,155 @@
+import argparse
+
+import numpy as np
+
+import chainer
+import chainer.functions as F
+import chainer.links as L
+from chainer import training
+from chainer.training import extensions
+
+class ParalellSequentialIterator(chainer.dataset.Iteretor):
+
+    def __init__(self, dataset, batch_size, repeat=True):
+        self.dataset = dataset
+        self.batch_size = batch_size
+        self.epoch = 0
+        self.is_new_epoch = False
+        self.repeat = repeat
+        length = len(dataset)
+        self.offsets = [i * length // batch_size for i in range(batch_size)]
+        self.iteration = 0
+
+    def __next__(self):
+        length = len(dataset)
+        if not self.repeat and self.iteration * self.batch_size >= length:
+            raise StopIteretion
+        cur_words = get_words()
+        self.iteration += 1
+        next_words = get_words()
+
+        epoch = self.iteration * self.batch_size // length
+        self.is_new_epoch = self.epoch < epoch
+        if self.is_new_epoch:
+            self.epoch = epoch
+
+        return list(zip(cur_words, next_words))
+
+    @property
+    def epoch_detail(self):
+        return self.iteration * self.batch_size / len(self.dataset)
+
+    def get_words(self):
+        return [self.dataset[(offset + self.iteration) % len(self.dataset)] for offset in self.offsets]
+
+    def serialize(self):
+        self.iteration = serializer('iteration', self.iteration)
+        self.epoch = serializer('epoch', self.epoch)
+
+class BPTTUpdater(training.StandardUpdater):
+
+    def __init__(self, train_iter, optimizer, bprop_len, device):
+        super(BPTTUpdater, self).__init__(
+                train_iter, optimizer, device=device
+                )
+        self.bprop_len = bprop_len
+
+    def update_core(self):
+        loss = 0
+        train_iter = self.get_iterator('main')
+        optimizer = self.get_optimizer('main')
+
+        for i in range(self.bprop_len):
+            batch = train_iter.__next__()
+            x, t = self.converter(batch, self.device)
+            loss += optimizer.target(chainer.Variable(x), chainer.Variable(t))
+
+        optimizer.target.cleargrads()
+        loss.backward()
+        loss.unchain_backward()
+        optimizer.update()
+
+def compute_perplexity(result):
+    result['perplexity'] = np.exp(result['main/loss'])
+    if 'validation/main/loss' in result:
+        result['val_perplexity'] = np.exp(result['validation/main/loss'])
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--batchsize', '-b', type=int, default=20,
+                        help='Number of examples in each mini-batch')
+    parser.add_argument('--bproplen', '-l', type=int, default=35,
+                        help='Number of words in each mini-batch '
+                             '(= length of truncated BPTT)')
+    parser.add_argument('--epoch', '-e', type=int, default=39,
+                        help='Number of sweeps over the dataset to train')
+    parser.add_argument('--gpu', '-g', type=int, default=-1,
+                        help='GPU ID (negative value indicates CPU)')
+    parser.add_argument('--gradclip', '-c', type=float, default=5,
+                        help='Gradient norm threshold to clip')
+    parser.add_argument('--out', '-o', default='result',
+                        help='Directory to output the result')
+    parser.add_argument('--resume', '-r', default='',
+                        help='Resume the training from snapshot')
+    parser.add_argument('--test', action='store_true',
+                        help='Use tiny datasets for quick tests')
+    parser.set_defaults(test=False)
+    parser.add_argument('--unit', '-u', type=int, default=650,
+                        help='Number of LSTM units in each layer')
+    args = parser.parse_args()
+
+    train, val, test = cainer.datasets.get_ptb_words()
+    n_vocab = max(train) + 1
+    print('#vocab =', n_vocab)
+
+    train_iter = ParallelSequentialIterator(train, args.batchsize)
+    val_iter = ParallelSequentialIterator(val, 1, repeat=False)
+    test_iter = ParallelSequentialIterator(test, 1, repeat=False)
+
+    rnn = RNNForLM(n_vocab, args.unit)
+    model = L.Classifier(rnn)
+    model.compute_accuracy = False
+    if args.gpu >= 0:
+        chainer.cuda.get_device(args.gpu).use()
+        model.tu_gpu()
+
+    optimizer = chainer.optimizers.SGD(lr=1.0)
+    optimizer.setup(model)
+    optimizer.add_hook(chainer.optimizer.GradientClipping(args.gradclip))
+
+    updater = BPTTUpdater(train_iter, optimizer, args.bproplen, args.gpu)
+    trainer = training.Trainer(updater, (args.epoch, 'epoch'), out=args.out)
+
+    eval_model = model.copy()
+    eval_rnn = eval_model.predictor
+    eval_rnn.train = False
+    trainer.extend(extensions.Evaluator(
+        val_iter, eval_model, device=args.gpu,
+        eval_hook=lambda _: eval_rnn.reset_state()))
+    interval = 10 if args.test else 500
+
+    trainer.extend(extensions.LogReport(postprocess=compute_perplexity,
+                                        trigger=(interval, 'iteration')))
+    trainer.extend(extensions.PrintReport(
+        ['epoch', 'iteration', 'perplexity', 'val_perplexity']
+    ), trigger=(interval, 'iteration'))
+    trainer.extend(extensions.ProgressBar(
+        update_interval=1 if args.test else 10))
+    trainer.extend(extensions.snapshot())
+    trainer.extend(extensions.snapshot_object(
+        model, 'model_iter_{.updater.iteration}'))
+    if args.resume:
+        chainer.serializers.load_npz(args.resume, trainer)
+
+    trainer.run()
+
+    print('test')
+    eval_rnn.reset_state()
+    evaluator = extensions.Evaluator(test_iter, eval_model, device=args.gpu)
+    result = evaluator()
+    print('test perplexity:', np.exp(float(result['main/loss'])))
+
+
+if __name__ == '__main__':
+    main()
+
